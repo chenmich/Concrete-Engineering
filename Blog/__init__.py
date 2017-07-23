af3bc751a0da30009b7a723a0e294a72f57aaddc
chenmich <403189920@qq.com>
2017-07-24 00:11:14 +0800

    add init_app in Blog/__init__.py

--- Blog/__init__.py
+++ Blog/__init__.py
@@ -1,12 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, Flask
 from .views import index
 from flask_bootstrap import Bootstrap
+from.config import config
 
 
 def create_blueprint():
     blog_blueprint = Blueprint('blog', __name__,
                                template_folder='templates',
                                static_folder='static')
-    
+  
     blog_blueprint.add_url_rule('/', endpoint=None, view_func=index)
     return blog_blueprint
+
+def init_app(app):
+    bootstrap = Bootstrap(app)
+    if  not isinstance(app, Flask):
+        raise TypeError('app is not an instance of class Flask')
+    app.register_blueprint(create_blueprint())
+
+
--- runblog.py
+++ runblog.py
@@ -1,10 +1,9 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
-from Blog import create_blueprint
+from Blog import create_blueprint,init_app
+
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
-app.register_blueprint(create_blueprint())
+init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
